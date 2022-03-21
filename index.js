4ee9adad168311dff800059d7deb0fa692578501
root <naiduiitkgp21@gmail.com>
2022-03-21 19:38:48 +0000

    updated code complete colution

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addDays = require("date-fns/addDays");
 const updateDate = (days) => {
-  const date = new Date(2020, 08, 22);
+  let date = new Date(2020, 8, 22);
   const newDate = addDays(date, days);
   return `${newDate.getDate()}-${newDate.getMonth()}-${newDate.getFullYear()}`;
 };
